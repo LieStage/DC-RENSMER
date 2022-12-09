0ee189dd606fd6ffb8ce4f10abe156102186fdfa
LieStage <118051769+LieStage@users.noreply.github.com>
2022-12-09 18:44:01 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,5 +3,25 @@
 from dotenv import load_dotenv
 import bot.client
 
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+
+async def root_route_handler(request):
+
+    return web.json_response("TRUMBOTS BY @FLIFHER")
+
+async def web_server():
+
+    web_app = web.Application(client_max_size=30000000)
+
+    web_app.add_routes(routes)
+
+    return web_app
+
+
+
 load_dotenv()
 bot = bot.client.Client()
