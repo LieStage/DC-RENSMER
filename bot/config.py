77749890aba3b68c1f1469436740f112b3dc9e3b
LieStage <118051769+LieStage@users.noreply.github.com>
2022-12-09 19:01:19 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,14 @@
+import os
+import logging
+
+logging.basicConfig(
+    format='%(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('log.txt'),
+              logging.StreamHandler()],
+    level=logging.INFO
+)
+
+
+class Config(object):
+
+PORT = int
